7c8bf3ca3af949f47539c159fd796021b079625f
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-13 19:49:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodId = 0
+let mealId = 0
 
 class Neighborhood{
   constructor(name){
     this.name = name 
     this.id = ++neighborhoodId
+  }
+
+}
+
+class Meal{
+  constructor(title, price){
+  this.title = title
+  this.price = price
+  this.id = ++mealID
   }
 }
