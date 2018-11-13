0871605dda11a44e1412392fa903b9b8bad85827
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-13 19:46:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborhoodId = 0
+
+class Neighborhood{
+  constructor(name){
+    this.name = name 
+    this.id = ++neighborhoodId
+  }
+}
